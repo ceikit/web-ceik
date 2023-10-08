9b9678bccf6f40c9f44efe9f355764b742068578
ceikit <filippoperugini91@gmail.com>
2023-10-08 10:40:37 +0000

    s

--- src/App.svelte
+++ src/App.svelte
@@ -1,80 +1,57 @@
 <script>
-    let email = 'dddd@email.com';
+  import 'bootstrap/dist/css/bootstrap.min.css';
+  import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+  let email = 'dddd@email.com';
 </script>
 
-<style>
-    body {
-        font-family: 'Helvetica', sans-serif;
-        color: #F2F2F2;
-		background: url('/44.jpg') no-repeat center center fixed !important;
-        background-size: cover; /* This will cover the entire viewport */
-        margin: 0;
-    }
-
-    nav {
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-        padding: 20px 50px;
-        background-color: #131B26;
-        color: #F2F2F2;
-    }
-
-    section {
-        padding: 50px 0;
-        text-align: center;
-    }
-
-    a {
-        color: #F2F2F2;
-        text-decoration: none;
-    }
-
-    ul {
-        list-style-type: none;
-        padding: 0;
-    }
-
-    li {
-        margin: 10px 0;
-    }
-</style>
-
-<nav>
-    <h1>Radon Tech</h1>
+<!-- Navbar -->
+<nav class="navbar navbar-dark bg-dark">
+  <span class="navbar-brand mb-0 h1">Radon Tech</span>
 </nav>
 
-<main>
-    <!-- About Section -->
-    <section>
-        <h2>About</h2>
-        <p>Risk Management and Investment Solutions for the Crypto industry.</p>
-    </section>
+<!-- Main Content -->
+<div class="container mt-5 text-light" style="background-color: #131B26;">
+  <!-- About Section -->
+  <section class="my-5">
+    <h2>About</h2>
+    <p>Risk Management and Investment Solutions for the Crypto industry.</p>
+  </section>
 
-    <!-- Services Section -->
-    <section>
-        <h2>Services</h2>
-        <ul>
-            <li>Risk management and yield enhancement</li>
-            <li>Market making</li>
-            <li>Optimal execution</li>
-        </ul>
-    </section>
+  <!-- Services Section -->
+  <section class="my-5">
+    <h2>Services</h2>
+    <ul class="list-unstyled">
+      <li>Risk management and yield enhancement</li>
+      <li>Market making</li>
+      <li>Optimal execution</li>
+    </ul>
+  </section>
 
-    <!-- Products Section -->
-    <section>
-        <h2>Products</h2>
-        <ul>
-            <li>Covered call</li>
-            <li>Covered put</li>
-            <li>Accumulator</li>
-            <li>Range farmer</li>
-        </ul>
-    </section>
+  <!-- Products Section -->
+  <section class="my-5">
+    <h2>Products</h2>
+    <ul class="list-unstyled">
+      <li>Covered call</li>
+      <li>Covered put</li>
+      <li>Accumulator</li>
+      <li>Range farmer</li>
+    </ul>
+  </section>
 
-    <!-- Contact Section -->
-    <section>
-        <h2>Contact</h2>
-        <p>Email us at: <a href="mailto:{email}">{email}</a></p>
-    </section>
-</main>
+  <!-- Contact Section -->
+  <section class="my-5">
+    <h2>Contact</h2>
+    <p>Email us at: <a href="mailto:{email}" class="text-light">{email}</a></p>
+  </section>
+</div>
+
+<!-- Background Image -->
+<style>
+  body {
+    font-family: 'Helvetica', sans-serif;
+    color: #F2F2F2;
+    background: url('/44.jpg') no-repeat center center fixed;
+    background-size: cover;
+    margin: 0;
+  }
+</style>
