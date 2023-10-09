37387cbc462f4fad30df1b0192ae57921dfe31af
filippo <filippoperugini91@gmail.com>
2023-10-09 13:56:01 +0200

    s

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,11 @@
 <script lang="ts">
 	import "../app.postcss";
-	import { AppShell, AppBar } from "@skeletonlabs/skeleton";
+	import {
+		AppShell,
+		AppBar,
+		TreeView,
+		TreeViewItem,
+	} from "@skeletonlabs/skeleton";
 
 	// Floating UI for Popups
 	import {
@@ -26,6 +31,188 @@
 					<strong class="text-xl uppercase">Radon Tech</strong>
 				</a>
 			</svelte:fragment>
+			<!-- <svelte:fragment slot="trail">
+				<a
+					class="btn btn-sm variant-ghost-surface"
+					href="https://discord.gg/EXqV7W8MtY"
+					target="_blank"
+					rel="noreferrer"
+				>
+					Discord
+				</a>
+				<a
+					class="btn btn-sm variant-ghost-surface"
+					href="https://twitter.com/SkeletonUI"
+					target="_blank"
+					rel="noreferrer"
+				>
+					Twitter
+				</a>
+				<a
+					class="btn btn-sm variant-ghost-surface"
+					href="https://github.com/skeletonlabs/skeleton"
+					target="_blank"
+					rel="noreferrer"
+				>
+					GitHub
+				</a>
+			</svelte:fragment> -->
+			<!-- <svelte:fragment slot="trail">
+				<a
+					class="btn btn-sm variant-ghost-surface"
+					href="https://discord.gg/EXqV7W8MtY"
+					target="_blank"
+					rel="noreferrer"
+				>
+					Discord
+				</a>
+				<a
+					class="btn btn-sm variant-ghost-surface"
+					href="https://twitter.com/SkeletonUI"
+					target="_blank"
+					rel="noreferrer"
+				>
+					Twitter
+				</a>
+				<a
+					class="btn btn-sm variant-ghost-surface"
+					href="https://github.com/skeletonlabs/skeleton"
+					target="_blank"
+					rel="noreferrer"
+				>
+					GitHub
+				</a>
+			</svelte:fragment> -->
+			<!-- <svelte:fragment slot="trail">
+				<a
+					class="btn btn-sm variant-ghost-surface"
+					href="https://discord.gg/EXqV7W8MtY"
+					target="_blank"
+					rel="noreferrer"
+				>
+					Discord
+				</a>
+				<a
+					class="btn btn-sm variant-ghost-surface"
+					href="https://twitter.com/SkeletonUI"
+					target="_blank"
+					rel="noreferrer"
+				>
+					Twitter
+				</a>
+				<a
+					class="btn btn-sm variant-ghost-surface"
+					href="https://github.com/skeletonlabs/skeleton"
+					target="_blank"
+					rel="noreferrer"
+				>
+					GitHub
+				</a>
+			</svelte:fragment> -->
+			<!-- <svelte:fragment slot="trail">
+				<a
+					class="btn btn-sm variant-ghost-surface"
+					href="https://discord.gg/EXqV7W8MtY"
+					target="_blank"
+					rel="noreferrer"
+				>
+					Discord
+				</a>
+				<a
+					class="btn btn-sm variant-ghost-surface"
+					href="https://twitter.com/SkeletonUI"
+					target="_blank"
+					rel="noreferrer"
+				>
+					Twitter
+				</a>
+				<a
+					class="btn btn-sm variant-ghost-surface"
+					href="https://github.com/skeletonlabs/skeleton"
+					target="_blank"
+					rel="noreferrer"
+				>
+					GitHub
+				</a>
+			</svelte:fragment> -->
+			<!-- <svelte:fragment slot="trail">
+				<a
+					class="btn btn-sm variant-ghost-surface"
+					href="https://discord.gg/EXqV7W8MtY"
+					target="_blank"
+					rel="noreferrer"
+				>
+					Discord
+				</a>
+				<a
+					class="btn btn-sm variant-ghost-surface"
+					href="https://twitter.com/SkeletonUI"
+					target="_blank"
+					rel="noreferrer"
+				>
+					Twitter
+				</a>
+				<a
+					class="btn btn-sm variant-ghost-surface"
+					href="https://github.com/skeletonlabs/skeleton"
+					target="_blank"
+					rel="noreferrer"
+				>
+					GitHub
+				</a>
+			</svelte:fragment> -->
+			<!-- <svelte:fragment slot="trail">
+				<a
+					class="btn btn-sm variant-ghost-surface"
+					href="https://discord.gg/EXqV7W8MtY"
+					target="_blank"
+					rel="noreferrer"
+				>
+					Discord
+				</a>
+				<a
+					class="btn btn-sm variant-ghost-surface"
+					href="https://twitter.com/SkeletonUI"
+					target="_blank"
+					rel="noreferrer"
+				>
+					Twitter
+				</a>
+				<a
+					class="btn btn-sm variant-ghost-surface"
+					href="https://github.com/skeletonlabs/skeleton"
+					target="_blank"
+					rel="noreferrer"
+				>
+					GitHub
+				</a>
+			</svelte:fragment> -->
+			<!-- <svelte:fragment slot="trail">
+				<a
+					class="btn btn-sm variant-ghost-surface"
+					href="https://discord.gg/EXqV7W8MtY"
+					target="_blank"
+					rel="noreferrer"
+				>
+					Discord
+				</a>
+				<a
+					class="btn btn-sm variant-ghost-surface"
+					href="https://twitter.com/SkeletonUI"
+					target="_blank"
+					rel="noreferrer"
+				>
+					Twitter
+				</a>
+				<a
+					class="btn btn-sm variant-ghost-surface"
+					href="https://github.com/skeletonlabs/skeleton"
+					target="_blank"
+					rel="noreferrer"
+				>
+					GitHub
+				</a>
+			</svelte:fragment> -->
 			<!-- <svelte:fragment slot="trail">
 				<a
 					class="btn btn-sm variant-ghost-surface"
@@ -81,15 +268,48 @@
 			<svelte:fragment slot="trail">
 				<!-- Adjust, add or remove these links as needed -->
 				<div class="option-bar">
-					<a class="btn btn-sm variant-ghost-surface" href="/about">About</a>
-					<a class="btn btn-sm variant-ghost-surface" href="/services">Services</a>
-					<a class="btn btn-sm variant-ghost-surface" href="/products">Products</a>
-					<a class="btn btn-sm variant-ghost-surface" href="/contact">Contact</a>
+					<a class="btn btn-sm variant-ghost-surface" href="/about"
+						>About</a
+					>
+					<a class="btn btn-sm variant-ghost-surface" href="/services"
+						>Services</a
+					>
+					<a class="btn btn-sm variant-ghost-surface" href="/products"
+						>Products</a
+					>
+					<a class="btn btn-sm variant-ghost-surface" href="/contact"
+						>Contact</a
+					>
 				</div>
-				<a class="btn btn-sm variant-ghost-surface" href="/about">About</a>
-				<a class="btn btn-sm variant-ghost-surface" href="/services">Services</a>
-				<a class="btn btn-sm variant-ghost-surface" href="/products">Products</a>
-				<a class="btn btn-sm variant-ghost-surface" href="/contact">Contact</a>
+				<a class="btn btn-sm variant-ghost-surface" href="/about"
+					>About
+				</a>
+				<a class="btn btn-sm variant-ghost-surface" href="/services"
+					>Services
+				</a>
+				<TreeView class="btn btn-sm variant-ghost-surface">
+					<a 
+					href="/products"> Products </a>
+					<TreeViewItem>
+						<svelte:fragment slot="children">
+							<TreeViewItem>
+								<a
+									class="btn btn-sm variant-ghost-surface"
+									href="/products/covered_call"
+									>Covered Call
+								</a>
+							</TreeViewItem>
+							<TreeViewItem>Accumulator</TreeViewItem>
+						</svelte:fragment>
+					</TreeViewItem>
+				</TreeView>
+				<a class="btn btn-sm variant-ghost-surface" href="/products"
+					>Products
+				</a>
+
+				<a class="btn btn-sm variant-ghost-surface" href="/contact"
+					>Contact</a
+				>
 			</svelte:fragment>
 		</AppBar>
 	</svelte:fragment>
