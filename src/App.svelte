4dbfdaa85adb0d2186737a7307c006e29d6d2192
ceikit <filippoperugini91@gmail.com>
2023-10-09 09:48:19 +0000

    s

--- src/App.svelte
+++ src/App.svelte
@@ -1,57 +1,63 @@
 <script>
-  import 'bootstrap/dist/css/bootstrap.min.css';
-  import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-  let email = 'dddd@email.com';
+    import 'bootstrap/dist/css/bootstrap.min.css';
+    let email = 'dddd@email.com';
 </script>
 
 <!-- Navbar -->
-<nav class="navbar navbar-dark bg-dark">
-  <span class="navbar-brand mb-0 h1">Radon Tech</span>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container">
+        <a class="navbar-brand" href="#">Radon Tech</a>
+    </div>
 </nav>
 
 <!-- Main Content -->
-<div class="container mt-5 text-light" style="background-color: #131B26;">
-  <!-- About Section -->
-  <section class="my-5">
-    <h2>About</h2>
-    <p>Risk Management and Investment Solutions for the Crypto industry.</p>
-  </section>
+<div class="container mt-5 py-5 text-light" style="background-color: #131B26; border-radius: 10px;">
+    <!-- About Section -->
+    <section class="mb-5">
+        <h2 class="display-4">About</h2>
+        <p class="lead">Risk Management and Investment Solutions for the Crypto industry.</p>
+    </section>
 
-  <!-- Services Section -->
-  <section class="my-5">
-    <h2>Services</h2>
-    <ul class="list-unstyled">
-      <li>Risk management and yield enhancement</li>
-      <li>Market making</li>
-      <li>Optimal execution</li>
-    </ul>
-  </section>
+    <!-- Services Section -->
+    <section class="mb-5">
+        <h2 class="display-4">Services</h2>
+        <ul class="list-unstyled mt-4">
+            <li class="lead">Risk management and yield enhancement</li>
+            <li class="lead">Market making</li>
+            <li class="lead">Optimal execution</li>
+        </ul>
+    </section>
 
-  <!-- Products Section -->
-  <section class="my-5">
-    <h2>Products</h2>
-    <ul class="list-unstyled">
-      <li>Covered call</li>
-      <li>Covered put</li>
-      <li>Accumulator</li>
-      <li>Range farmer</li>
-    </ul>
-  </section>
+    <!-- Products Section -->
+    <section class="mb-5">
+        <h2 class="display-4">Products</h2>
+        <ul class="list-unstyled mt-4">
+            <li class="lead">Covered call</li>
+            <li class="lead">Covered put</li>
+            <li class="lead">Accumulator</li>
+            <li class="lead">Range farmer</li>
+        </ul>
+    </section>
 
-  <!-- Contact Section -->
-  <section class="my-5">
-    <h2>Contact</h2>
-    <p>Email us at: <a href="mailto:{email}" class="text-light">{email}</a></p>
-  </section>
+    <!-- Contact Section -->
+    <section class="mb-5">
+        <h2 class="display-4">Contact</h2>
+        <p class="lead mt-4">Email us at: <a href="mailto:{email}" class="text-info">{email}</a></p>
+    </section>
 </div>
 
 <!-- Background Image -->
 <style>
-  body {
-    font-family: 'Helvetica', sans-serif;
-    color: #F2F2F2;
-    background: url('/44.jpg') no-repeat center center fixed;
-    background-size: cover;
-    margin: 0;
-  }
+    body {
+        font-family: 'Helvetica', sans-serif;
+        color: #F2F2F2;
+        background: url('/44.jpg') no-repeat center center fixed;
+        background-size: cover;
+        margin: 0;
+        height: 100vh;
+    }
+
+    .display-4 {
+        font-size: 2rem;
+    }
 </style>
