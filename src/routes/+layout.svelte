fba04b35b2bbfbf6fa8a34b97a62ff6f0f179f19
filippo <filippoperugini91@gmail.com>
2023-10-09 13:39:13 +0200

    s

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,15 +1,20 @@
 <script lang="ts">
-	import '../app.postcss';
-	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
+	import "../app.postcss";
+	import { AppShell, AppBar } from "@skeletonlabs/skeleton";
 
 	// Floating UI for Popups
-	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
-	import { storePopup } from '@skeletonlabs/skeleton';
+	import {
+		computePosition,
+		autoUpdate,
+		flip,
+		shift,
+		offset,
+		arrow,
+	} from "@floating-ui/dom";
+	import { storePopup } from "@skeletonlabs/skeleton";
 	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
 	let isFocused: boolean = true;
-
 </script>
-
 
 <!-- App Shell -->
 <AppShell scrollbarGutter="auto">
@@ -17,7 +22,9 @@
 		<!-- App Bar -->
 		<AppBar>
 			<svelte:fragment slot="lead">
-				<strong class="text-xl uppercase">Radon Tech</strong>
+				<a href="/">
+					<strong class="text-xl uppercase">Radon Tech</strong>
+				</a>
 			</svelte:fragment>
 			<!-- <svelte:fragment slot="trail">
 				<a
@@ -45,12 +52,44 @@
 					GitHub
 				</a>
 			</svelte:fragment> -->
+			<style>
+				/* Hide the trail fragment on screens smaller than 640px */
+				@media (max-width: 640px) {
+					[slot="trail"] {
+						display: none;
+					}
+				}
+
+				/* Style the option bar */
+				.option-bar {
+					display: none;
+					position: absolute;
+					top: 100%;
+					right: 0;
+					background-color: white;
+					box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
+					padding: 0.5rem;
+					z-index: 1;
+				}
+
+				.option-bar a {
+					display: block;
+					margin-bottom: 0.5rem;
+				}
+			</style>
+
 			<svelte:fragment slot="trail">
 				<!-- Adjust, add or remove these links as needed -->
+				<div class="option-bar">
+					<a class="btn btn-sm variant-ghost-surface" href="/about">About</a>
+					<a class="btn btn-sm variant-ghost-surface" href="/services">Services</a>
+					<a class="btn btn-sm variant-ghost-surface" href="/products">Products</a>
+					<a class="btn btn-sm variant-ghost-surface" href="/contact">Contact</a>
+				</div>
 				<a class="btn btn-sm variant-ghost-surface" href="/about">About</a>
-				<a class="btn btn-sm variant-ghost-surface" href="#services">Services</a>
-				<a class="btn btn-sm variant-ghost-surface" href="#products">Products</a>
-				<a class="btn btn-sm variant-ghost-surface" href="#contact">Contact</a>
+				<a class="btn btn-sm variant-ghost-surface" href="/services">Services</a>
+				<a class="btn btn-sm variant-ghost-surface" href="/products">Products</a>
+				<a class="btn btn-sm variant-ghost-surface" href="/contact">Contact</a>
 			</svelte:fragment>
 		</AppBar>
 	</svelte:fragment>
--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,8 +1,12 @@
 <script lang="ts">
+    import { RangeSlider } from '@skeletonlabs/skeleton';
     import type { PageData } from './$types';
     
     export let data: PageData;
+    let value = 15;
+
 </script>
 
 <h1>About Us</h1>
 <p>Ciao patatella baby amore mio grande</p>
+<RangeSlider name="range-slider" bind:value={value} max={25} step={1} ticked>Label</RangeSlider>
