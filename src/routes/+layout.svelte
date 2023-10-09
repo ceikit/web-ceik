abb79afe384f51963f2131bfa32a6175c9a78cd4
filippo <filippoperugini91@gmail.com>
2023-10-09 12:31:05 +0200

    s

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -16,7 +16,7 @@
 			<svelte:fragment slot="lead">
 				<strong class="text-xl uppercase">Radon Tech</strong>
 			</svelte:fragment>
-			<svelte:fragment slot="trail">
+			<!-- <svelte:fragment slot="trail">
 				<a
 					class="btn btn-sm variant-ghost-surface"
 					href="https://discord.gg/EXqV7W8MtY"
@@ -41,7 +41,7 @@
 				>
 					GitHub
 				</a>
-			</svelte:fragment>
+			</svelte:fragment> -->
 			<svelte:fragment slot="trail">
 				<!-- Adjust, add or remove these links as needed -->
 				<a class="btn btn-sm variant-ghost-surface" href="#about">About</a>
