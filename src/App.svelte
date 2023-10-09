9a3da486f9b61ffd1ea08d86c82062c8d80fac90
ceikit <filippoperugini91@gmail.com>
2023-10-09 10:01:51 +0000

    s

--- src/App.svelte
+++ src/App.svelte
@@ -1,57 +1,56 @@
+<head>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/skeleton/2.0.4/skeleton.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.0.3/dist/tailwind.min.css" rel="stylesheet">
+</head>
+
+
 <script>
     let email = 'dddd@email.com';
 </script>
 
-<head>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/skeleton/2.0.4/skeleton.min.css">
-</head>
-
 <!-- Navbar -->
-<div class="container">
+<div class="container mx-auto mt-6">
     <div class="row">
-        <div class="twelve columns u-pull-right">
-            <h1 class="u-pull-left">Radon Tech</h1>
+        <div class="twelve columns">
+            <h1 class="text-3xl text-white font-bold">Radon Tech</h1>
         </div>
     </div>
 </div>
 
 <!-- Main Content -->
-<div class="container" style="background-color: #131B26; padding: 3em; border-radius: 7px;">
+<div class="container mx-auto mt-12 p-8 rounded-lg" style="background-color: #131B26;">
 
     <!-- About Section -->
-    <section>
-        <h2>About</h2>
-        <p>Risk Management and Investment Solutions for the Crypto industry.</p>
-        <hr>
+    <section class="mb-8">
+        <h2 class="text-2xl text-white mb-4">About</h2>
+        <p class="text-white leading-relaxed">Risk Management and Investment Solutions for the Crypto industry.</p>
     </section>
 
     <!-- Services Section -->
-    <section>
-        <h2>Services</h2>
-        <ul class="square">
-            <li>Risk management and yield enhancement</li>
-            <li>Market making</li>
-            <li>Optimal execution</li>
+    <section class="mb-8">
+        <h2 class="text-2xl text-white mb-4">Services</h2>
+        <ul class="list-disc pl-5 space-y-2">
+            <li class="text-white">Risk management and yield enhancement</li>
+            <li class="text-white">Market making</li>
+            <li class="text-white">Optimal execution</li>
         </ul>
-        <hr>
     </section>
 
     <!-- Products Section -->
-    <section>
-        <h2>Products</h2>
-        <ul class="square">
-            <li>Covered call</li>
-            <li>Covered put</li>
-            <li>Accumulator</li>
-            <li>Range farmer</li>
+    <section class="mb-8">
+        <h2 class="text-2xl text-white mb-4">Products</h2>
+        <ul class="list-disc pl-5 space-y-2">
+            <li class="text-white">Covered call</li>
+            <li class="text-white">Covered put</li>
+            <li class="text-white">Accumulator</li>
+            <li class="text-white">Range farmer</li>
         </ul>
-        <hr>
     </section>
 
     <!-- Contact Section -->
     <section>
-        <h2>Contact</h2>
-        <p>Email us at: <a href="mailto:{email}">{email}</a></p>
+        <h2 class="text-2xl text-white mb-4">Contact</h2>
+        <p class="text-white leading-relaxed">Email us at: <a href="mailto:{email}" class="underline text-blue-400">{email}</a></p>
     </section>
 
 </div>
@@ -59,23 +58,9 @@
 <style>
     body {
         font-family: 'Helvetica', sans-serif;
-        color: #F2F2F2;
         background: url('/44.jpg') no-repeat center center fixed;
         background-size: cover;
         margin: 0;
         height: 100vh;
-    }
-    
-    a {
-        color: #F2F2F2;
-    }
-    
-    hr {
-        border-top: 2px solid #F2F2F2;
-        margin: 2em 0;
-    }
-
-    h1, h2 {
-        color: #F2F2F2;
     }
 </style>
